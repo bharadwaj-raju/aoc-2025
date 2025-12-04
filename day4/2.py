3dfb1cc647d529e527e23f0065c117358676e3e6
Bharadwaj Raju <bharadwaj.raju777@gmail.com>
2025-12-04 15:08:07 +0530

    day 4: only check neighbors of removed rolls

--- day4/2.py
+++ day4/2.py
@@ -3,20 +3,29 @@ from util import readgrid, vec2, grid_get
 grid = readgrid()
 
 
-def get_reachable(grid) -> list[vec2]:
+def can_reach(grid, pos: vec2) -> bool:
+    return len([nb for nb in pos.all_neighbors() if grid_get(grid, nb, ".") == "@"]) < 4
+
+
+def get_reachable(grid, only_check: set[vec2] | None = None) -> tuple[list[vec2], set[vec2]]:
     reachable = []
-    for y, row in enumerate(grid):
-        for x, cell in enumerate(row):
-            if cell == ".":
-                continue
-            pos = vec2(x, y)
-            if len([nb for nb in pos.all_neighbors() if grid_get(grid, nb, ".") == "@"]) < 4:
-                reachable.append(pos)
-    return reachable
+    neighbors = set()
+    if only_check is None:
+        only_check = {vec2(x, y) for y in range(len(grid)) for x in range(len(grid[0]))}
+    for pos in only_check:
+        cell = grid_get(grid, pos, ".")
+        if cell == ".":
+            continue
+        if can_reach(grid, pos):
+            reachable.append(pos)
+            neighbors |= {nb for nb in pos.all_neighbors() if grid_get(grid, nb, ".") == "@"}
+    return reachable, neighbors
 
 
 all_removed = []
-while now_removed := get_reachable(grid):
+neighbors = None
+while (removed_and_neighbors := get_reachable(grid, neighbors))[0]:
+    now_removed, neighbors = removed_and_neighbors
     all_removed.extend(now_removed)
     for rm in now_removed:
         grid[rm.y][rm.x] = "."
